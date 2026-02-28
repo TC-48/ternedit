56c0fee035661da2766cf520fe7593341731e870
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-02-28 10:30:32 +0100

    refactor: add minimum window size and TritState data array
    - Added MIN_WIN_WIDTH and MIN_WIN_HEIGHT constants
    - Modified SDL_Window creation to be resizable
    - Added TritState data array and rendering loop
    - Enforced minimum window size on resize events

--- src/main.c
+++ src/main.c
@@ -7,12 +7,20 @@
 #define DEFAULT_WIN_WIDTH 640
 #define DEFAULT_WIN_HEIGHT 480
 
+#define MIN_WIN_WIDTH 200
+#define MIN_WIN_HEIGHT 150
+
 #define DEFAULT_FONT_SIZE 88
 
+typedef uint8_t TritState;
+
 int main() {
+    TritState data[1024] = { 1, 0, 2, 1, 0, 1, 1, 2, 0, 1, 2, 2, 2, 1, 0, 2 };
+    size_t count = 16;
+
     SDL_Init(SDL_INIT_VIDEO);
     TTF_Init();
-    SDL_Window* window = SDL_CreateWindow("ternedit", 0, 0, DEFAULT_WIN_WIDTH, DEFAULT_WIN_HEIGHT, 0);
+    SDL_Window* window = SDL_CreateWindow("ternedit", 0, 0, DEFAULT_WIN_WIDTH, DEFAULT_WIN_HEIGHT, SDL_WINDOW_RESIZABLE);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     TTF_Font* regular = TTF_OpenFont("assets/fonts/monogram/ttf/monogram.ttf", DEFAULT_FONT_SIZE);
@@ -30,14 +38,31 @@ int main() {
             switch (event.type) {
             case SDL_QUIT:
                 running = SDL_FALSE;
+                break;
+            case SDL_WINDOWEVENT:
+                if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
+                    int w = event.window.data1;
+                    int h = event.window.data2;
+
+                    // enforce minimum size
+                    if (w < MIN_WIN_WIDTH || h < MIN_WIN_HEIGHT) {
+                        if (w < MIN_WIN_WIDTH) w = MIN_WIN_WIDTH;
+                        if (h < MIN_WIN_HEIGHT) h = MIN_WIN_HEIGHT;
+                        SDL_SetWindowSize(window, w, h);
+                    }
+                }
+                break;
             default:;
             }
         }
 
-
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, atlas.chars[REGULAR][0], NULL, &(SDL_Rect) { .x = 0, .y = 0, .w = atlas.charWidth, .h = atlas.charHeight });
+        SDL_Rect rect = { .x = 0, .y = 0, .w = atlas.charWidth, .h = atlas.charHeight };
+        for (size_t i = 0; i < count; ++i) {
+            SDL_RenderCopy(renderer, atlas.chars[REGULAR][data[i]], NULL, &rect);
+            rect.x += atlas.charWidth + ((i+1) % 6 == 0 ? 17 : 0);
+        }
         SDL_RenderPresent(renderer);
     }
     
